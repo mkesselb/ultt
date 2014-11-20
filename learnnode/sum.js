fe1ce7b2af5215c34977ced05b7b78476894e2b5
mkesselb <mkesse@gmx.at>
2014-11-20 10:57:57 +0100

    deleting unnecessary folders

--- learnnode/sum.js
+++
@@ -1,5 +0,0 @@
-sum = 0;
-for(i = 2; i < process.argv.length; i++){
-	sum += parseInt(process.argv[i]);
-}
-console.log(sum);
--- nodejs/learnnode/http.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-http.get(process.argv[2], function(response){
-	response.on("error", function(err){
-		console.log("Error: " + err.toString());});
-	response.on("data", function(data){
-		console.log(data.toString());});
-	});
