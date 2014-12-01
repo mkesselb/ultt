6bea327956f3ea7e9100c63cda1d073c88eb0b94
comoessl <comoessl@edu.uni-klu.ac.at>
2014-12-01 11:33:47 +0100

    quiz form: read data from input form (not yet saved to object/db)

--- client/Assets/Scripts/Panels/PanelFormQuiz.cs
+++ client/Assets/Scripts/Panels/PanelFormQuiz.cs
@@ -12,8 +12,7 @@ public class PanelFormQuiz : MonoBehaviour {
 	public int question_id;
 
 	//text fields
-	public List<List<Text>> inputFields;
-
+	public List<List<GameObject>> answers;
 	public GameObject btnAddQuestion;
 	public GameObject btnSave;
 
@@ -31,15 +30,17 @@ public class PanelFormQuiz : MonoBehaviour {
 
 	public void init(){
 		questions = new List<GameObject> ();
+		answers = new List<List<GameObject>> ();
 		question_id = 0;
 		addQuestionForm();
 
 	}
 
-	public void addAnswerForm(string questionName){
+	public void addAnswerForm(string questionName, int id){
 		Debug.Log ("Add answer");
 		GameObject generatedAnswer = Instantiate (answer, Vector3.zero, Quaternion.identity) as GameObject;
 		generatedAnswer.transform.parent = GameObject.Find(questionName+"/answers").transform;
+		answers [id].Add (generatedAnswer);
 	}
 
 	public void addQuestionForm(){
@@ -49,20 +50,36 @@ public class PanelFormQuiz : MonoBehaviour {
 		int id = question_id;
 		generatedQuestion.name = "question" + id;
 		generatedQuestion.transform.parent = GameObject.Find ("panelQuestions/questions").transform;
-		generatedQuestion.transform.FindChild("ButtonAdd").GetComponent<Button>().onClick.AddListener (() => {addAnswerForm(generatedQuestion.name);});	
+		generatedQuestion.transform.FindChild("ButtonAdd").GetComponent<Button>().onClick.AddListener (() => {addAnswerForm(generatedQuestion.name, id);});	
 		questions.Add (generatedQuestion);
+		List<GameObject> answersForQuestion = new List<GameObject> ();
+		answers.Add (answersForQuestion);
+		addAnswerForm (generatedQuestion.name, id);
+		addAnswerForm (generatedQuestion.name, id);
 		question_id++;
 	}
 
 	public void saveQuestions(){
-		foreach (GameObject q in questions) {
-			Debug.Log ("questionText: "+q.transform.FindChild("InputField/Text").GetComponent<Text>().text);
+		for (int i = 0; i < questions.Count; i++){
+			//save question text
+			Debug.Log ("Question: "+ questions[i].transform.FindChild("InputField/Text").GetComponent<Text>().text);
 
-			Transform[] answers = q.transform.Find("answers").GetComponentsInChildren<Transform>();
-
-			foreach (Transform answer in answers){
-				Debug.Log ("answer: "+answer.FindChild("InputField/Text").GetComponent<Text>().text);
+			//save answers
+			List<GameObject> answerList = answers[i];
+			foreach(GameObject obj in answerList){
+				Debug.Log ("answer: "+obj.transform.FindChild("InputField/Text").GetComponent<Text>().text);
 			}
+		}
+
+
+//		foreach (GameObject q in questions) {
+//			Debug.Log ("questionText: "+q.transform.FindChild("InputField/Text").GetComponent<Text>().text);
+//
+//			Transform[] answers = q.transform.Find("answers").GetComponentsInChildren<Transform>();
+//
+//			foreach (Transform answer in answers){
+//				Debug.Log ("answer: "+answer.FindChild("InputField/Text").GetInstanceID());
+//			}
 
 
 //			Transform[] allChildren = q.transform.GetComponentsInChildren<Transform>();
@@ -71,7 +88,7 @@ public class PanelFormQuiz : MonoBehaviour {
 //					Debug.Log ("Question: "+tr.GetComponent<Text>().text);
 //				}
 //			}
-		}
+//		}
 
 
 	}
